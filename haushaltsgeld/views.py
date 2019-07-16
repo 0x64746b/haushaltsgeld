64dd389f6e29bc64eeb2b807c54d4348d4eafe3d
dtk <dtk@gmx.de>
2019-07-17 00:43:21 +0200

    Commit expenses to database
    Use a dummy user until flask login has been integrated.

--- haushaltsgeld/views.py
+++ haushaltsgeld/views.py
@@ -3,6 +3,7 @@
 from flask import Blueprint, redirect, render_template, request, url_for
 
 from haushaltsgeld.forms import ExpenseForm
+from haushaltsgeld.models import Expense, User, db
 
 expenses = Blueprint('expenses', __name__)
 
@@ -12,8 +13,14 @@ def add_expense():
     form = ExpenseForm()
 
     if request.method == 'POST' and form.validate():
-        print(form.amount.data, type(form.amount.data))
-        print(form.store.data, type(form.store.data))
+        db.session.add(
+            Expense(
+                user=User.query.first(),  # TODO: Use user from session
+                amount=form.amount.data,
+                store=form.store.data
+            )
+        )
+        db.session.commit()
         return redirect(url_for('expenses.list_expenses'))
 
     return render_template('add.html', form=form)
