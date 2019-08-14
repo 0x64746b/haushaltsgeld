d5e2b24b8bbdccd8f3be8c9574985ca662d58e7c
dtk <dtk@gmx.de>
2019-08-14 14:30:33 +0200

    Transport authenticated user in login form
    Don't resolve the provided identifier twice. We wouldn't want it to
    resolve to two different entities.

--- haushaltsgeld/auth/forms.py
+++ haushaltsgeld/auth/forms.py
@@ -4,12 +4,12 @@ from flask_wtf import FlaskForm
 from wtforms import SubmitField, StringField, PasswordField
 from wtforms.validators import InputRequired, ValidationError
 
-from haushaltsgeld.auth.models import resolve_user
+from haushaltsgeld.auth.models import resolve_user, User
 
 
 class LoginForm(FlaskForm):
 
-    INVALID_CREDENTIALS_MSG = 'Username or password are invalid'
+    authenticated_user: User = None
 
     username = StringField(
         'Username',
@@ -24,4 +24,7 @@ class LoginForm(FlaskForm):
     def validate_password(self, field):
         user = resolve_user(self.username.data)
         if not user or not user.is_correct_password(self.password.data):
-            raise ValidationError(self.INVALID_CREDENTIALS_MSG)
+            self.authenticated_user = None
+            raise ValidationError('Username or password are invalid')
+
+        self.authenticated_user = user
--- haushaltsgeld/auth/views.py
+++ haushaltsgeld/auth/views.py
@@ -5,7 +5,6 @@ from flask_login import login_user, logout_user
 from werkzeug.utils import redirect
 
 from haushaltsgeld.auth.forms import LoginForm
-from haushaltsgeld.auth.models import resolve_user
 from . import auth
 
 
@@ -14,7 +13,7 @@ def login():
     form = LoginForm()
 
     if form.validate_on_submit():
-        login_user(resolve_user(form.username.data))  # The form validation checked the credentials
+        login_user(form.authenticated_user)  # The form validation checked the credentials
         return redirect(current_app.config['INDEX_PAGE'])
 
     return render_template('login.html', form=form)
