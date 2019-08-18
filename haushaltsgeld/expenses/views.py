3f2ff47173f4f18d1dccfa6cd9fc29c827ad8a49
dtk <dtk@gmx.de>
2019-08-18 19:56:22 +0200

    Make expenses editable

--- haushaltsgeld/expenses/views.py
+++ haushaltsgeld/expenses/views.py
@@ -1,8 +1,6 @@
 # coding: utf-8
 
-from datetime import date
-
-from flask import redirect, render_template, url_for
+from flask import redirect, render_template, request, url_for
 from flask_login import login_required, current_user
 
 from . import expenses
@@ -24,6 +22,24 @@ def add_expense():
                 date=form.date.data,
             )
         )
+        _db.session.commit()
+        return redirect(url_for('expenses.list_expenses'))
+
+    return render_template('add.html', form=form)
+
+
+@expenses.route('/edit/<id>', methods=['GET', 'POST'])
+@login_required
+def edit_expense(id):
+    expense = Expense.query.get(id)
+    form = ExpenseForm(obj=expense)
+
+    if form.validate_on_submit():
+        expense.amount = form.amount.data
+        expense.store = form.store.data
+        expense.date = form.date.data
+
+        _db.session.add(expense)
         _db.session.commit()
         return redirect(url_for('expenses.list_expenses'))
 
